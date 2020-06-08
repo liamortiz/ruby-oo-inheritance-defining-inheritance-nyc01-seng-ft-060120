73daf6df220cec893cbc9f7a3ee67c7b3b2dab23
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-08 17:47:22 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,13 @@
 require_relative "./vehicle.rb"
+require 'pry'
+
+class Car < Vehicle
+  def initialize(wheel_size, wheel_number)
+    super(wheel_size, wheel_number)
+  end
+  
+  def go
+    'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+  end
+  
+end
